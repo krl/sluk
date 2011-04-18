krl/sluk

commit 537dc59c7c922a0b7f4d1a2732d171076dfcf05f

use unique maildir-style filenames for writing out messages
Read http://cr.yp.to/proto/maildir.html

sluk.py
@@ -2,9 +2,9 @@
 # -*- mode: Python; encoding: utf-8; indent-tabs-mode: nil; tab-width: 2 -*-
 
 import ConfigParser
-from hashlib import md5
 import os
 import time
+import socket
 from email.mime.text import MIMEText
 from email.utils import formatdate
 import json
@@ -16,6 +16,11 @@ def print_optionally(string):
   "print the given string if the config option quiet is false or not set"
   if not conf.has_option("conf", "quiet") or not conf.getboolean("conf", "quiet"):
     print string
+
+def create_unique_filename():
+  "Create a unique maildir-style filename. See http://cr.yp.to/proto/maildir.html"
+  filename = repr(time.time()) + "_" + str(os.getpid()) + "." + socket.gethostname() + ":2,"
+  return filename
 
 # initialize user config
 conf = ConfigParser.ConfigParser()
@@ -101,16 +106,18 @@ for feed in open(conf.get("conf", "feed_list")).read().split("\n"):
     if not os.path.exists(directory):
       os.makedirs(directory)
 
-    path = os.path.join(directory, lnk.replace("/", "!"))
+    # we like unique filenames
+    path = ""
+    while not path or os.path.exists(path):
+      path = os.path.join(directory, create_unique_filename())
 
-    # python don't like long pathnames
-    if len(path) > 256:
-      path = os.path.join(conf.get("conf", "messages"), md5(path).hexdigest())
+    # this should never ever occur (although never say never ever),
+    # but leave it here anyway since removing it would alter indentation
+    # for about 40 lines, and we don't like obese patches for simple changes
+    if os.path.exists(path):
+      print("E: File already exists: '%s'  -- Skipping..." % path)
 
-    # ignore updated feeds for now
-    # maybe TODO handle this in any way?
-    if not os.path.exists(path):
-
+    else:
       # content is not always in feed, use summary
       if "content" in entry:
         content = entry.content[0].value

commit 0bd44a41672fc47047913c0a0b0c8a6fb8448413

don't write the same entry more than once (episode 2)
fixes a bug introduced in 537dc59c7c922a0b

URLs of processed entries are now written (appended)
to a file of which the name is autogenerated based on

    conf.get("conf", "cache")

        so if "cache" is
            "/home/user/.sluk_cache"

        the entry log ends up in
            "/home/user/.sluk_cache_entries"

and incoming entries are looked up in this list to
verify that they don't already exist locally.

sluk.py
@@ -43,6 +43,22 @@ try:
 except IOError, ValueError:
   cache = {}
 
+
+# Simple string concatenation instead of os.path.join(),
+# we want "cache_entries", not "cache/_entries" !
+cache_entries_file = conf.get("conf", "cache") + "_entries"
+
+try:
+  # "with" ensures everything is nicely cleaned up afterwards
+  # (closed, released, ...) without needing specific "finally:" code.
+  with open(cache_entries_file, 'r') as f:
+    cache_entries = f.read().split("\n")
+except IOError:
+  cache_entries = ""
+
+cache_entries_new = ""
+
+
 entries = []
 
 for feed in open(conf.get("conf", "feed_list")).read().split("\n"):
@@ -101,6 +117,14 @@ for feed in open(conf.get("conf", "feed_list")).read().split("\n"):
         continue # If the entry has neither link nor href element, it's clearly not a feed -- skip it.
     else:
       lnk = entry.link
+
+    # If lnk is NOT in cache_entries, append it to
+    # cache_entries_new and proceed as usual.
+    # Otherwise, drop this entry and start processing the next.
+    if not lnk in cache_entries:
+      cache_entries_new += lnk + "\n"
+    else:
+      continue
 
     directory = os.path.join(conf.get("conf", "messages"), (nick or ""))
     if not os.path.exists(directory):
@@ -180,3 +204,10 @@ print_optionally("updating cache file: " + conf.get("conf", "cache"))
 cache_file = open(conf.get("conf", "cache"), "w")
 cache_file.write(json.dumps(cache))
 cache_file.close()
+
+try:
+  print_optionally("I: Updating entries cache: '%s'" % cache_entries_file)
+  with open(cache_entries_file, 'a') as f:  # append, not write
+    f.write(cache_entries_new)
+except IOError:
+  print_optionally("E: Failed writing to entries cache file: '%s'" % cache_entries_file)