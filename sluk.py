krl/sluk

commit 537dc59c7c922a0b7f4d1a2732d171076dfcf05f

use unique maildir-style filenames for writing out messages
Read http://cr.yp.to/proto/maildir.html

sluk.py
@@ -2,9 +2,9 @@
 # -*- mode: Python; encoding: utf-8; indent-tabs-mode: nil; tab-width: 2 -*-
 
 import ConfigParser
-from hashlib import md5
 import os
 import time
+import socket
 from email.mime.text import MIMEText
 from email.utils import formatdate
 import json
@@ -16,6 +16,11 @@ def print_optionally(string):
   "print the given string if the config option quiet is false or not set"
   if not conf.has_option("conf", "quiet") or not conf.getboolean("conf", "quiet"):
     print string
+
+def create_unique_filename():
+  "Create a unique maildir-style filename. See http://cr.yp.to/proto/maildir.html"
+  filename = repr(time.time()) + "_" + str(os.getpid()) + "." + socket.gethostname() + ":2,"
+  return filename
 
 # initialize user config
 conf = ConfigParser.ConfigParser()
@@ -101,16 +106,18 @@ for feed in open(conf.get("conf", "feed_list")).read().split("\n"):
     if not os.path.exists(directory):
       os.makedirs(directory)
 
-    path = os.path.join(directory, lnk.replace("/", "!"))
+    # we like unique filenames
+    path = ""
+    while not path or os.path.exists(path):
+      path = os.path.join(directory, create_unique_filename())
 
-    # python don't like long pathnames
-    if len(path) > 256:
-      path = os.path.join(conf.get("conf", "messages"), md5(path).hexdigest())
+    # this should never ever occur (although never say never ever),
+    # but leave it here anyway since removing it would alter indentation
+    # for about 40 lines, and we don't like obese patches for simple changes
+    if os.path.exists(path):
+      print("E: File already exists: '%s'  -- Skipping..." % path)
 
-    # ignore updated feeds for now
-    # maybe TODO handle this in any way?
-    if not os.path.exists(path):
-
+    else:
       # content is not always in feed, use summary
       if "content" in entry:
         content = entry.content[0].value

commit 92c1ef5b7db9b5b6e1965a0027667ef2390e6ebe

don't write the same entry more than once
Duct Tape fix for a bug introduced in 537dc59c7c922a0b

URLs of processed entries are now appended to a file defined by a new
config option named "cache_entries", and the "cache" config option was
renamed to "cache_feeds" to avoid confusion.

Failsafes are in place to prevent breakage due to "old" config files.

Long day + short coding session = ugly, bloated patch (yet works as advertised)

README.org
@@ -13,12 +13,13 @@ feedparser
 On invocation the program will look for a configuration file path in $SLUK_CONFIG (if set) or fall back to "~/.slukrc", which should contain something like this:
 
 : [conf]
-: messages  = /home/user/mail/sluk/
-: feed_list = /home/user/.sluk_feeds
-: cache     = /home/user/.sluk_cache
-: quiet     = true
+: messages      = /home/user/mail/sluk/
+: feed_list     = /home/user/.sluk_feeds
+: cache_feeds   = /home/user/.sluk_cache_feeds
+: cache_entries = /home/user/.sluk_cache_entries
+: quiet         = true
 : [filters]
-: readproxy = readproxy {url}
+: readproxy     = readproxy {url}
 
 If the option "quiet" is either missing or set to "false", sluk will babble away mercilessly.
 

sluk.py
@@ -38,10 +38,33 @@ except IOError:
   exit(1)
 
 # initialize cache
+if not conf.has_option("conf", "cache_feeds"):
+  cache_feeds_file = conf.get("conf", "cache")
+else:
+  cache_feeds_file = conf.get("conf", "cache_feeds")
+
+if not conf.has_option("conf", "cache_entries"):
+  cache_entries_file = conf.get("conf", "cache") + "_entries"
+else:
+  cache_entries_file = conf.get("conf", "cache_entries")
+
 try:
-  cache = json.loads(open(conf.get("conf", "cache")).read())
+  with open(cache_feeds_file, 'r') as f:
+    print_optionally("I: Using feeds cache file: '%s'" % cache_feeds_file)
+    cache = json.loads(f.read())
 except IOError, ValueError:
+  print_optionally("E: Failed loading feeds cache file: '%s'" % cache_feeds_file)
   cache = {}
+
+try:
+  with open(cache_entries_file, 'r') as f:
+    print_optionally("I: Using entries cache file: '%s'" % cache_entries_file)
+    cache_entries = f.read().split("\n")
+except IOError:
+  print_optionally("E: Failed loading entries cache file: '%s'" % cache_entries_file)
+  cache_entries = ""
+
+cache_entries_new = ""
 
 entries = []
 
@@ -101,6 +124,11 @@ for feed in open(conf.get("conf", "feed_list")).read().split("\n"):
         continue # If the entry has neither link nor href element, it's clearly not a feed -- skip it.
     else:
       lnk = entry.link
+
+    if not lnk in cache_entries:
+      cache_entries_new += lnk + "\n"
+    else:
+      continue
 
     directory = os.path.join(conf.get("conf", "messages"), (nick or ""))
     if not os.path.exists(directory):
@@ -176,7 +204,16 @@ for x in entries:
 
 # update cache
 
-print_optionally("updating cache file: " + conf.get("conf", "cache"))
-cache_file = open(conf.get("conf", "cache"), "w")
-cache_file.write(json.dumps(cache))
-cache_file.close()
+try:
+  print_optionally("I: Updating feeds cache: '%s'" % cache_feeds_file)
+  with open(cache_feeds_file, 'w') as f:
+    f.write(json.dumps(cache))
+except IOError:
+  print_optionally("E: Failed writing to feeds cache file: '%s'" % cache_feeds_file)
+
+try:
+  print_optionally("I: Updating entries cache: '%s'" % cache_entries_file)
+  with open(cache_entries_file, 'a') as f:
+    f.write(cache_entries_new)
+except IOError:
+  print_optionally("E: Failed writing to entries cache file: '%s'" % cache_entries_file)

slukrc-example
@@ -1,4 +1,6 @@
 [conf]
-messages  = /home/user/mail/sluk/
-feed_list = /home/user/.sluk_feeds
-cache     = /home/user/.sluk_cache
+messages      = /home/user/mail/sluk/
+feed_list     = /home/user/.sluk_feeds
+cache_feeds   = /home/user/.sluk_cache_feeds
+cache_entries = /home/user/.sluk_cache_entries
+quiet         = false