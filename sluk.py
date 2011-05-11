73a7a755d53bb0fbb4ecd971a08a387974003b44
Jameson Graef Rollins <jrollins@finestructure.net>
2011-05-11 01:35:58 -0700

    move code to update all feeds into sub-function
    This looks like a big change, but it simply moves all of the feed
    updating code into a sub-function.  This is paving the way to
    integrate a new subcommand ui.

--- sluk.py
+++ sluk.py
@@ -11,6 +11,8 @@ import json
 import commands
 import feedparser
 
+##################################################
+
 # function definitions
 def print_optionally(string):
   "print the given string if the config option quiet is false or not set"
@@ -21,6 +23,180 @@ def create_unique_filename():
   "Create a unique maildir-style filename. See http://cr.yp.to/proto/maildir.html"
   filename = repr(time.time()) + "_" + str(os.getpid()) + "." + socket.gethostname() + ":2,"
   return filename
+
+def update_feeds():
+  "update all feeds"
+  # initialize cache
+  try:
+    cache = json.loads(open(conf.get("conf", "cache")).read())
+  except IOError, ValueError:
+    cache = {}
+
+  # Simple string concatenation instead of os.path.join(),
+  # we want "cache_entries", not "cache/_entries" !
+  cache_entries_file = conf.get("conf", "cache") + "_entries"
+
+  try:
+    # "with" ensures everything is nicely cleaned up afterwards
+    # (closed, released, ...) without needing specific "finally:" code.
+    with open(cache_entries_file, 'r') as f:
+      cache_entries = f.read().split("\n")
+  except IOError:
+    cache_entries = ""
+
+  cache_entries_new = ""
+
+  entries = []
+
+  for feed in open(conf.get("conf", "feed_list")).read().split("\n"):
+
+    # means commented out
+    if not feed or feed[0] == "#": continue
+
+    split      = feed.split()
+    nick       = None
+    bodyfilter = None
+
+    if len(split) > 1:
+      feed = split[1]
+      nick = split[0].decode("utf-8")
+
+    if len(split) > 2:
+      bodyfilter = split[2]
+
+    if nick:
+      print_optionally(nick + " " + feed)
+    else:
+      print_optionally(feed)
+
+    if feed not in cache:
+      cache[feed] = {"etag": None, "modified": None}
+
+    try:
+      parsed = feedparser.parse(feed,
+                                etag     = cache[feed]["etag"],
+                                # needs time in tuple form
+                                modified = cache[feed]["modified"] and \
+                                  time.gmtime(cache[feed]["modified"]))
+    except:
+      print_optionally("parsing failed!")
+      continue
+
+    if 'status' in parsed and parsed.status == 304:
+      print_optionally(" - server says not changed")
+      continue
+
+    cache[feed]["etag"]     = parsed.etag if hasattr(parsed, "etag") else None
+    cache[feed]["modified"] = time.mktime(parsed.modified) if hasattr(parsed, "modified") else None
+
+    # count
+    num_written = 0
+
+    for entry in parsed.entries:
+      lnk = ""
+      if 'link' not in entry:
+        if 'enclosures' in entry and 'href' in entry.enclosures[0]:
+          lnk = entry.enclosures[0].href
+        else:
+          print_optionally("Warning! Skipping entry in feed %s that lacks both href enclosure and entry element!" % feed)
+          continue # If the entry has neither link nor href element, it's clearly not a feed -- skip it.
+      else:
+        lnk = entry.link
+
+      # If lnk is NOT in cache_entries, append it to
+      # cache_entries_new and proceed as usual.
+      # Otherwise, drop this entry and start processing the next.
+      if not lnk in cache_entries:
+        cache_entries_new += lnk + "\n"
+      else:
+        continue
+
+      directory = os.path.join(conf.get("conf", "messages"), (nick or ""))
+      if not os.path.exists(directory):
+        os.makedirs(directory)
+
+      # we like unique filenames
+      path = ""
+      while not path or os.path.exists(path):
+        path = os.path.join(directory, create_unique_filename())
+
+      # this should never ever occur (although never say never ever),
+      # but leave it here anyway since removing it would alter indentation
+      # for about 40 lines, and we don't like obese patches for simple changes
+      if os.path.exists(path):
+        print("E: File already exists: '%s'  -- Skipping..." % path)
+      else:
+        # content is not always in feed, use summary
+        if "content" in entry:
+          content = entry.content[0].value
+        elif "summary" in entry:
+          content = entry.summary
+        else:
+          content = lnk
+
+        try:
+          content   = content.encode(parsed.encoding)
+          feed_name = parsed['feed']['title'].encode(parsed.encoding)
+          title     = entry['title'].encode(parsed.encoding)
+          link      = lnk.encode(parsed.encoding)
+        except UnicodeEncodeError:
+          print_optionally("error decoding entry: " + path)
+          continue
+        except KeyError:
+          print_optionally("error parsing entry: " + path)
+          continue
+
+        if bodyfilter:
+          content = commands.getoutput(conf.get("filters", bodyfilter).replace("{url}", link))
+
+        # create text/html message only
+        msg = MIMEText(content, "html")
+
+        msg['Subject'] = title
+        msg['From']    = feed_name + " <sluk@" + os.uname()[1] + ">"
+        msg['To']      = "sluk@" + os.uname()[1]
+
+        if hasattr(entry, "updated_parsed"):
+          msg['Date']  = formatdate(time.mktime(entry.updated_parsed))
+        else:
+          msg['Date']  = formatdate(time.time())
+
+        msg['X-Entry-URL'] = msg['Message-ID'] = link
+
+        # write to file
+        entries.append({"path": path,
+                        "body": msg.as_string()})
+
+        num_written += 1
+
+    if num_written == 1:
+      print_optionally(" - 1 new entry")
+    if num_written > 1:
+      print_optionally(" - %i new entries" % num_written)
+
+  # write files
+
+  for x in entries:
+    print_optionally("writing " + x['path'])
+    message_file = open(x['path'], "w")
+    message_file.write(x['body'])
+    message_file.close()
+
+  # update cache
+
+  print_optionally("updating cache file: " + conf.get("conf", "cache"))
+  cache_file = open(conf.get("conf", "cache"), "w")
+  cache_file.write(json.dumps(cache))
+  cache_file.close()
+
+  try:
+    print_optionally("I: Updating entries cache: '%s'" % cache_entries_file)
+    with open(cache_entries_file, 'a') as f:  # append, not write
+      f.write(cache_entries_new)
+  except IOError:
+    print_optionally("E: Failed writing to entries cache file: '%s'" % cache_entries_file)
+
+##################################################
 
 # initialize user config
 conf = ConfigParser.ConfigParser()
@@ -37,177 +213,4 @@ except IOError:
   print("E: Config file not found '%s'" % config_file)
   exit(1)
 
-# initialize cache
-try:
-  cache = json.loads(open(conf.get("conf", "cache")).read())
-except IOError, ValueError:
-  cache = {}
-
-
-# Simple string concatenation instead of os.path.join(),
-# we want "cache_entries", not "cache/_entries" !
-cache_entries_file = conf.get("conf", "cache") + "_entries"
-
-try:
-  # "with" ensures everything is nicely cleaned up afterwards
-  # (closed, released, ...) without needing specific "finally:" code.
-  with open(cache_entries_file, 'r') as f:
-    cache_entries = f.read().split("\n")
-except IOError:
-  cache_entries = ""
-
-cache_entries_new = ""
-
-
-entries = []
-
-for feed in open(conf.get("conf", "feed_list")).read().split("\n"):
-
-  # means commented out
-  if not feed or feed[0] == "#": continue
-
-  split      = feed.split()
-  nick       = None
-  bodyfilter = None
-
-  if len(split) > 1:
-    feed = split[1]
-    nick = split[0].decode("utf-8")
-
-  if len(split) > 2:
-    bodyfilter = split[2]
-
-
-  if nick:
-    print_optionally(nick + " " + feed)
-  else:
-    print_optionally(feed)
-
-  if feed not in cache:
-    cache[feed] = {"etag": None, "modified": None}
-
-  try:
-    parsed = feedparser.parse(feed,
-                              etag     = cache[feed]["etag"],
-                              # needs time in tuple form
-                              modified = cache[feed]["modified"] and \
-                                time.gmtime(cache[feed]["modified"]))
-  except:
-    print_optionally("parsing failed!")
-    continue
-
-  if 'status' in parsed and parsed.status == 304:
-    print_optionally(" - server says not changed")
-    continue
-
-  cache[feed]["etag"]     = parsed.etag if hasattr(parsed, "etag") else None
-  cache[feed]["modified"] = time.mktime(parsed.modified) if hasattr(parsed, "modified") else None
-
-  # count
-  num_written = 0
-
-  for entry in parsed.entries:
-
-    lnk = ""
-    if 'link' not in entry:
-      if 'enclosures' in entry and 'href' in entry.enclosures[0]:
-        lnk = entry.enclosures[0].href
-      else:
-        print_optionally("Warning! Skipping entry in feed %s that lacks both href enclosure and entry element!" % feed)
-        continue # If the entry has neither link nor href element, it's clearly not a feed -- skip it.
-    else:
-      lnk = entry.link
-
-    # If lnk is NOT in cache_entries, append it to
-    # cache_entries_new and proceed as usual.
-    # Otherwise, drop this entry and start processing the next.
-    if not lnk in cache_entries:
-      cache_entries_new += lnk + "\n"
-    else:
-      continue
-
-    directory = os.path.join(conf.get("conf", "messages"), (nick or ""))
-    if not os.path.exists(directory):
-      os.makedirs(directory)
-
-    # we like unique filenames
-    path = ""
-    while not path or os.path.exists(path):
-      path = os.path.join(directory, create_unique_filename())
-
-    # this should never ever occur (although never say never ever),
-    # but leave it here anyway since removing it would alter indentation
-    # for about 40 lines, and we don't like obese patches for simple changes
-    if os.path.exists(path):
-      print("E: File already exists: '%s'  -- Skipping..." % path)
-
-    else:
-      # content is not always in feed, use summary
-      if "content" in entry:
-        content = entry.content[0].value
-      elif "summary" in entry:
-        content = entry.summary
-      else:
-        content = lnk
-
-      try:
-        content   = content.encode(parsed.encoding)
-        feed_name = parsed['feed']['title'].encode(parsed.encoding)
-        title     = entry['title'].encode(parsed.encoding)
-        link      = lnk.encode(parsed.encoding)
-      except UnicodeEncodeError:
-        print_optionally("error decoding entry: " + path)
-        continue
-      except KeyError:
-        print_optionally("error parsing entry: " + path)
-        continue
-
-      if bodyfilter:
-        content = commands.getoutput(conf.get("filters", bodyfilter).replace("{url}", link))
-
-      # create text/html message only
-      msg = MIMEText(content, "html")
-
-      msg['Subject'] = title
-      msg['From']    = feed_name + " <sluk@" + os.uname()[1] + ">"
-      msg['To']      = "sluk@" + os.uname()[1]
-
-      if hasattr(entry, "updated_parsed"):
-        msg['Date']  = formatdate(time.mktime(entry.updated_parsed))
-      else:
-        msg['Date']  = formatdate(time.time())
-
-      msg['X-Entry-URL'] = msg['Message-ID'] = link
-
-      # write to file
-      entries.append({"path": path,
-                      "body": msg.as_string()})
-
-      num_written += 1
-
-  if num_written == 1:
-    print_optionally(" - 1 new entry")
-  if num_written > 1:
-    print_optionally(" - %i new entries" % num_written)
-
-# write files
-
-for x in entries:
-  print_optionally("writing " + x['path'])
-  message_file = open(x['path'], "w")
-  message_file.write(x['body'])
-  message_file.close()
-
-# update cache
-
-print_optionally("updating cache file: " + conf.get("conf", "cache"))
-cache_file = open(conf.get("conf", "cache"), "w")
-cache_file.write(json.dumps(cache))
-cache_file.close()
-
-try:
-  print_optionally("I: Updating entries cache: '%s'" % cache_entries_file)
-  with open(cache_entries_file, 'a') as f:  # append, not write
-    f.write(cache_entries_new)
-except IOError:
-  print_optionally("E: Failed writing to entries cache file: '%s'" % cache_entries_file)
+update_feeds()
